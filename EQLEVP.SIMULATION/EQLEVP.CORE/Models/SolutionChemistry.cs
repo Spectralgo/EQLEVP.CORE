e4cde052639686485d2ffdc366f3f6c96289f263
spectralgo <florian.renard@spectralgo.com>
2023-03-24 20:14:19 +0100

    INFRA AND DATA: Project Init & SolutionChemistry

---
+++ EQLEVP.SIMULATION/EQLEVP.CORE/Models/SolutionChemistry.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Geologists
+// ---------------------------------------------------------------
+
+namespace EQLEVP.CORE.Models
+{
+    public class SolutionChemistry
+    {
+        public string Label { get; set; }
+        public double Temp { get; set; }
+        public double Dens { get; set; }
+        public double Ph { get; set; }
+        public double Na { get; set; }
+        public double K { get; set; }
+        public double Li { get; set; }
+        public double Ca { get; set; }
+        public double Mg { get; set; }
+        public double Cl { get; set; }
+        public double SO4 { get; set; }
+        public double NO3 { get; set; }
+        public double B { get; set; }
+        public double Si { get; set; }
+        public double Alk { get; set; }
+    }
+}
---
+++ EQLEVP.SIMULATION/EQLEVP.CORE/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EQLEVP.CORE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
